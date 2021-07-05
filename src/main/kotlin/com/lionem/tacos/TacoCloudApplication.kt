986485bf04aea66595ddbb38854e80aa65d4d217
Lion.M <lionem2018@gmail.com>
2021-07-05 11:17:32 +0100

    init commit

---
+++ src/main/kotlin/com/lionem/tacos/TacoCloudApplication.kt
@@ -0,0 +1,11 @@
+package com.lionem.tacos
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TacoCloudApplication
+
+fun main(args: Array<String>) {
+	runApplication<TacoCloudApplication>(*args)
+}

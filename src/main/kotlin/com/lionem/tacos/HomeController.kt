18a51558a1b49546d96b188347164ce6682976e5
lionem2018 <lionem2018@gmail.com>
2021-07-08 21:06:53 +0900

    add HomeController, home.html, HomeControllerTest

---
+++ src/main/kotlin/com/lionem/tacos/HomeController.kt
@@ -0,0 +1,11 @@
+package com.lionem.tacos
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HomeController {
+
+    @GetMapping("/")
+    fun home(): String = "home"
+}
---
+++ src/test/kotlin/com/lionem/tacos/HomeControllerTest.kt
@@ -0,0 +1,26 @@
+package com.lionem.tacos
+
+import org.hamcrest.Matchers.containsString
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
+@WebMvcTest(HomeController::class)
+class HomeControllerTest {
+
+    @Autowired
+    private var mockMvc: MockMvc? = null
+
+    @Test
+    fun testHomePage() {
+        mockMvc?.perform(get("/"))
+                ?.andExpect(status().isOk)
+                ?.andExpect(view().name("home"))
+                ?.andExpect(content().string(
+                        containsString("Welcome to...")
+                ))
+    }
+}
